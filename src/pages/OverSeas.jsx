2465b86801acd3f12543ebe78ac13a100f8b4bf5
Nirbhay Sisodia <nirbhaysis888@gmail.com>
2025-01-27 23:43:42 +0530

    commit error in Helper

--- src/pages/OverSeas.jsx
+++ src/pages/OverSeas.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import CountryList from '../components/CountryList'
 import Footer from '../components/Footer'
-import HelperContact from '../components/helperContact'
+import HelperContact from '../components/HelperContact'
 
 const Overseas = () => {
   return (
